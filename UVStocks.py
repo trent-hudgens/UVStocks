3d633ec1eb59bb81f2887ae5e974a58212d676d5
divadcrizap <68246591+divadcrizap@users.noreply.github.com>
2022-05-24 14:25:04 -0600

    added features to the stock page
    added cash held, shares owned, and score Labels and an amount Entry

--- UVStocks.py
+++ UVStocks.py
@@ -25,6 +25,10 @@ def main():
 
     logo_frm.pack()
 
+    cash_shares_frm = Frame(root)
+    Label(cash_shares_frm, text="Total Cash: xxx", fg="black", anchor="w").pack(side=LEFT, padx=50)
+    Label(cash_shares_frm, text="Total Shares Held: xxx", fg="black", anchor="e").pack(side=RIGHT, padx=50)
+    cash_shares_frm.pack()
 
     # Build and place the stock graph frame (dummy for now)
     graph_frm = Frame(root)
@@ -45,6 +49,28 @@ def main():
     b2.pack(side=RIGHT, padx=50)
 
     btn_frm.pack()
+
+    #entry field for amount of stocks to buy or sell
+    input_amount_frm = Frame(root)
+    inputAmount = Entry(input_amount_frm, bg="white", fg="black", width=100)
+    inputAmount.pack(side=LEFT)
+    inputAmount.insert(0, "Amount")
+    def clickInput(*args):
+        amountStr = str(inputAmount.get())
+        if amountStr == "Amount":
+            inputAmount.delete(0, 'end')
+    def focusOutInput(*args):
+        amountStr = str(inputAmount.get())
+        if amountStr == "":
+            inputAmount.insert(0, "Amount")
+            root.focus()
+    inputAmount.bind("<Button-1>", clickInput)
+    inputAmount.bind("<FocusOut>", focusOutInput)
+    input_amount_frm.pack()
+
+    score_frm = Frame(root)
+    Label(score_frm, text="SCORE: XXXXX.XX", fg="black", anchor="w").pack(side=LEFT)
+    score_frm.pack()
 
 
     root.mainloop()
