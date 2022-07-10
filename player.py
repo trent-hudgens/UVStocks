dde4eb79bb3e537c214393cf4846997ecb0f86e8
trenthudgens <hudgens.trent@gmail.com>
2022-07-10 10:35:55 -0600

    implemented back button. player persistence between switching frames is broken still.

--- player.py
+++ player.py
@@ -69,22 +69,14 @@ class Player:
     def update_leaderboards(self):
         """updates the leaderboards with the user's last score."""
 
-        # create leaderboard csv if it doesn't already exist
-        header = ['Name', 'Score']
-        if not os.path.exists('leaderboards.csv'):
-            with open('leaderboards.csv', 'w', newline="") as csvfile:
-                csvwriter = csv.writer(csvfile)
-                csvwriter.writerow(header)
-                for i in reversed(range(0, 10)):
-                    csvwriter.writerow(("BLANK", int(0)))
-
         # create list of tuples that represent leaderboard
+        # add the current user's latest score to the list
         with open("leaderboards.csv", 'r') as readerObj:
             next(readerObj)
             csv_reader = csv.reader(readerObj)
             tuplesList = list(map(tuple, csv_reader))
 
-            user_leaderboard_entry = (str(self.name), str(round(self.score, 2)))
+            user_leaderboard_entry = (str(self.name), str(round(self.score)))
             [tuplesList.pop(tuplesList.index(entry)) for entry in tuplesList if entry[0] == user_leaderboard_entry[0]]
             tuplesList.append(user_leaderboard_entry)
 
@@ -92,9 +84,9 @@ class Player:
             readerObj.close()
 
         # write new leaderboard
+        header = ['Name', 'Score']
         with open("leaderboards.csv", 'w', newline="") as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(header)
             for i in range(0, len(sortedTuples)):
                 csvwriter.writerow(sortedTuples[i])
-
