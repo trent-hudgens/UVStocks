d74bc814401abb83fe399ba0aadfdb9f6ed25b53
Kyler Draper <32275825+TheElementsOfGaming@users.noreply.github.com>
2022-08-02 20:11:05 -0600

    Revert "created dumby version of the StockTracker class"

--- stockdata.py
+++ stockdata.py
@@ -4,10 +4,10 @@ from stock_generator import stock_history
 class StockData:
     def __init__(self, history_size=500, start_price=500):
         self.stock_generator = stock_history(history_size, start_price)
-        self.price = next(self.stock_generator)
+        self.stock_price = next(self.stock_generator)
 
     def update_price(self):
-        self.price = next(self.stock_generator)
+        self.stock_price = next(self.stock_generator)
 
     def get_price(self):
-        return self.price
+        return self.stock_price
